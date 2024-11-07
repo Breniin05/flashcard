eb529317bc0adeca1b6348dd3ea0600e647b70f0
Breniin05 <brenobr.sud@gmail.com>
2024-11-06 22:17:17 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Política',
+    'Quem foi Gertulio Vargas?',
+    'Getúlio Dornelles Vargas GCTE GCA foi um militar, advogado e político brasileiro, líder da Revolução de 1930, que pôs fim à República Velha e depondo seu 13.º e último presidente, Washington Luís, impedindo a posse do presidente eleito em 1.º de março de 1930, Júlio Prestes.
+'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital de Portugual?',
+    'A capital de Portugual é Lisboa'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Futebol',
+    'Quem é Messi?',
+    'Lionel Andrés Messi Cuccittini é um futebolista argentino que atua como atacante. Atualmente, joga pelo Inter Miami e pela Seleção Argentina, onde, atuando como capitão, venceu a Copa do Mundo do Catar de 2022.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz vou embora em Inglês?',
+    'Vou embora em Iglês é I'm leaving'
 )
